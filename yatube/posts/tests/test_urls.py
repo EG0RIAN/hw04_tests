5d067eae5e1e4e9a0557ddef402913963a535d1f
Egor <e.arkhiptsev@gmail.com>
2022-11-17 01:03:51 +0300

    not ready yet

---
+++ yatube/posts/tests/test_urls.py
@@ -0,0 +1,121 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+from posts.models import Group, Post
+
+User = get_user_model()
+
+
+class PostsUrlsTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.group = Group.objects.create(
+            title='Название для теста',
+            slug='slug',
+            description='Описание для теста',
+        )
+        cls.author = User.objects.create_user(
+            username='Пользователь для теста'
+        )
+        cls.no_author = User.objects.create_user(
+            username='Не зареганный юзер'
+        )
+        cls.post = Post.objects.create(
+            author=cls.author,
+            text='Пост для теста',
+        )
+        cls.templates_url_names_public = {
+            'posts/index.html': reverse('posts:index'),
+            'posts/group_list.html': reverse(
+                'posts:group_list',
+                kwargs={'slug': cls.group.slug},
+            ),
+            'posts/profile.html': reverse(
+                'posts:profile',
+                kwargs={'username': cls.author.username},
+            ),
+        }
+
+        cls.templates_url_names_private = {
+            'posts/create_post.html': reverse('posts:create_post')
+        }
+
+        cls.templates_url_names = {
+            'posts/index.html': reverse('posts:index'),
+            'posts/group_list.html': reverse(
+                'posts:group_list',
+                kwargs={'slug': cls.group.slug},
+            ),
+            'posts/profile.html': reverse(
+                'posts:profile',
+                kwargs={'username': cls.author.username},
+            ),
+            'posts/create_post.html': reverse('posts:create_post'),
+        }
+
+    def setUp(self):
+        self.guest_client = Client()
+
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.author)
+
+        self.no_author_client = Client()
+        self.no_author_client.force_login(self.no_author)
+
+    def test_urls_guest_user_private(self):
+        """
+        Проверка на доступнотсь ссылок гостевому пользователю и редирект
+        недоступных страниц.
+        """
+        for template, reverse_name in self.templates_url_names_private.items():
+            with self.subTest():
+                response = self.guest_client.get(reverse_name)
+                self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        response = self.guest_client.get(
+            reverse(
+                'posts:post_edit',
+                kwargs={'post_id': self.post.id},
+            )
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+    def test_urls_guest_user_public(self):
+        """
+        Проверка на доступнотсь ссылок гостевому пользователю и редирект
+        доступных страниц.
+        """
+        for template, reverse_name in self.templates_url_names_public.items():
+            with self.subTest():
+                response = self.guest_client.get(reverse_name)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_urls_authorized_user(self):
+        """Проверка ссылок авторизованному пользователю - автору поста."""
+        for template, reverse_name in self.templates_url_names.items():
+            with self.subTest():
+                response = self.authorized_client.get(reverse_name)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_urls_no_authorized_user(self):
+        """Проверка ссылок авторизованному пользователю - не автору поста."""
+        for template, reverse_name in self.templates_url_names.items():
+            with self.subTest():
+                if reverse_name == reverse(
+                    'posts:post_edit',
+                    kwargs={'post_id': self.post.id},
+                ):
+                    response = self.no_author_client.get(reverse_name)
+                    self.assertEqual(response.status_code, HTTPStatus.FOUND)
+                else:
+                    response = self.no_author_client.get(reverse_name)
+                    self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_urls_use_correct_template(self):
+        """Проверка на то что URL-адрес использует подходящий шаблон."""
+        for template, reverse_name in self.templates_url_names.items():
+            with self.subTest():
+                response = self.authorized_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
---
+++ yatube/posts/views.py
@@ -0,0 +1,71 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import PostForm
+from .models import Group, Post, User
+from .utils import paginator
+
+
+def index(request):
+    post_list = Post.objects.all()
+    page_obj = paginator(request, post_list)
+    context = {'page_obj': page_obj, }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()
+    page_obj = paginator(request, posts)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.all()
+    page_obj = paginator(request, posts)
+    context = {
+        'page_obj': page_obj,
+        'author': author,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    context = {
+        'post': post,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def create_post(request):
+    form = PostForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            create_post = form.save(commit=False)
+            create_post.author = request.user
+            create_post.save()
+            return redirect('posts:profile', request.user.username)
+    context = {'form': form}
+    return render(request, 'posts/create_post.html', context)
+
+
+@login_required
+def post_edit(request, post_id):
+    posts = Post.objects.select_related('group')
+    edit_post = get_object_or_404(posts, id=post_id)
+    if request.user.id != edit_post.author_id:
+        return redirect('posts:post_detail', post_id)
+    form = PostForm(request.POST or None, instance=edit_post)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('posts:post_detail', post_id)
+    context = {'form': form, 'is_edit': True}
+    return render(request, 'posts/create_post.html', context)
